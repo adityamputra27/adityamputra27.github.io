b0779c95ff4f53ad0ea322d0a28128801464b13c
AdityaMuhamadPutraP <adityamuhamadputra@gmail.com>
2021-08-06 09:14:38 +0700

    finish project detail with carousel

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -51,3 +51,42 @@ open__modal.addEventListener('click', () => {
 close.addEventListener('click', () => {
     modal__container.classList.remove('show')
 })
+
+// Carouselnya
+const projectsSlide = document.querySelector('.projects__slide')
+const projectsImages = document.querySelectorAll('.projects__slide img')
+
+// Tombol prev dan next
+const prevBtn = document.querySelector('#prevBtn')
+const nextBtn = document.querySelector('#nextBtn')
+
+// Buat counter untuk slides nya
+let counter = 1
+const size = projectsImages[0].clientWidth
+projectsSlide.style.transform = 'translateX('+ (-size * counter) +'px)'
+
+prevBtn.addEventListener('click', function () {
+    projectsSlide.style.transition = "transform 0.5s ease-in"
+    counter--
+    projectsSlide.style.transform = 'translateX('+ (-size * counter) +'px)'
+})
+nextBtn.addEventListener('click', function () {
+    // console.log(-size)
+    // alert('TES')
+    projectsSlide.style.transition = "transform 0.5s ease-in"
+    counter++
+    projectsSlide.style.transform = 'translateX('+ (-size * counter) +'px)'
+})
+projectsSlide.addEventListener('transitionend', function () {
+    console.log(projectsImages[counter].id)
+    if (projectsImages[counter].id === "firstSlide") {
+        prevBtn.style.display = 'none'
+    } else {
+        prevBtn.style.display = 'block'
+    }
+    if (projectsImages[counter].id === "lastSlide") {
+        nextBtn.style.display = 'none'
+    } else {
+        nextBtn.style.display = 'block'
+    }
+})
